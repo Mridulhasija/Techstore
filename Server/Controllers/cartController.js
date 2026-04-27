a14c3bb8035fb598f3b11ce641fd8cd103e95253
Mridulhasija <mridulhasija11@gmail.com>
2026-04-28 00:40:52 +0530

    Update cartController.js

--- Server/Controllers/cartController.js
+++ Server/Controllers/cartController.js
@@ -1,24 +1,32 @@
 const db = require("../config/db");
 
+// Add item to cart
 exports.addToCart = (req, res) => {
-  const { userId, productId } = req.body;
+  const { userId, productId, quantity } = req.body;
 
   db.query(
-    "INSERT INTO cart (userId, productId) VALUES (?,?)",
-    [userId, productId],
+    "INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, ?)",
+    [userId, productId, quantity],
     (err, result) => {
-      if (err) throw err;
-      res.json({ message: "Added to cart" });
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.json({ message: "Item added to cart" });
     }
   );
 };
 
+// Get cart items
 exports.getCart = (req, res) => {
+  const userId = req.params.userId;
+
   db.query(
-    "SELECT * FROM cart WHERE userId=?",
-    [req.params.userId],
+    "SELECT * FROM cart WHERE user_id = ?",
+    [userId],
     (err, result) => {
-      if (err) throw err;
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
       res.json(result);
     }
   );
