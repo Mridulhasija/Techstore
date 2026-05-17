1c103937965208d167200315b5ef64446de5b6fd
Mridulhasija <mridulhasija11@gmail.com>
2026-05-18 04:54:27 +0530

    Update ProductsGrid.jsx

--- client/src/components/ProductsGrid.jsx
+++ client/src/components/ProductsGrid.jsx
@@ -1,18 +1,23 @@
-import ProductCard from "./ProductCard";
+import { useEffect, useState } from "react";
+import API from "../api/api";
 
-function ProductsGrid({ products, addToCart }) {
+function ProductsGrid() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    API.get("/api/products")
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <section className="section">
-      <div className="products-grid">
-        {products.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            addToCart={addToCart}
-          />
-        ))}
-      </div>
-    </section>
+    <div className="products-grid">
+      {products.map((product) => (
+        <div key={product.id}>
+          {product.name}
+        </div>
+      ))}
+    </div>
   );
 }
 
