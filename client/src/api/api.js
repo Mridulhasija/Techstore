b5e5de7c9f1a1d28ba0b89238109c121e3daeba0
Mridulhasija <mridulhasija11@gmail.com>
2026-05-18 17:04:57 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://techstore-trv3.onrender.com"
+const api = axios.create({
+  baseURL: "https://techstore-trv3.onrender.com",
 });
+
+
+export const getProducts    = ()    => api.get("/api/products");
+export const getByCategory  = (cat) => api.get(`/api/products/${cat}`);
+
+
+export const addToCart      = (userId, productId, quantity) =>
+  api.post("/api/cart/add", { userId, productId, quantity });
+export const getCart        = (userId) => api.get(`/api/cart/${userId}`);
+
+export default api;
