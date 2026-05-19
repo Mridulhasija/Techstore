e7b483ec0c2dce24b96628b85e2b03654a5d0f21
Mridulhasija <mridulhasija11@gmail.com>
2026-05-19 16:33:11 +0530

    Update db.js

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
-const db = mysql.createPool({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -10,6 +9,16 @@ const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  connectTimeout: 60000,
 });
 
-module.exports = db;
+connection.getConnection((err, conn) => {
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("MySQL Connected");
+    conn.release();
+  }
+});
+
+module.exports = connection;
