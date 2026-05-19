9b5cd06329d1cb153098935ba3dd1ca53628ac0a
Mridulhasija <mridulhasija11@gmail.com>
2026-05-19 17:50:39 +0530

    Update Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -29,5 +29,9 @@ setInterval(() => {
   });
 }, 14 * 60 * 1000); 
 
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
