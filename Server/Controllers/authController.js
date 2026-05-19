1bd44caa5017d9b41d55e4232f619f622e97e614
Mridulhasija <mridulhasija11@gmail.com>
2026-05-19 17:43:45 +0530

    Update authController.js

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -3,6 +3,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const SECRET = process.env.JWT_SECRET;
+if (!SECRET) {
+  throw new Error("JWT_SECRET missing");
+}
 
 exports.register = async (req, res) => {
   try {
