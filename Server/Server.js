3ea02272f6c12eacfe73dbb7835f43838947ef30
Mridulhasija <mridulhasija11@gmail.com>
2026-05-19 17:42:48 +0530

    Update Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -3,7 +3,12 @@ const cors    = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://techstore-f.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/",          (req, res) => res.send("API is running 🚀"));
