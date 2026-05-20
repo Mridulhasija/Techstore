53eca1e609b77a36922708ba6ed946251166007b
Mridulhasija <mridulhasija11@gmail.com>
2026-05-20 23:47:31 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
 import { BrowserRouter } from "react-router-dom";
+
 import { CartProvider } from "./context/CartContext";
 import { ToastProvider } from "./hooks/useToast";
+import { AuthProvider } from "./context/AuthContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-<BrowserRouter>
-<ToastProvider>
-<CartProvider>
-<App />
-</CartProvider>
-</ToastProvider>
-</BrowserRouter>
-</React.StrictMode>
+
+  <React.StrictMode>
+
+    <BrowserRouter>
+
+      <ToastProvider>
+
+        <AuthProvider>
+
+          <CartProvider>
+
+            <App />
+
+          </CartProvider>
+
+        </AuthProvider>
+
+      </ToastProvider>
+
+    </BrowserRouter>
+
+  </React.StrictMode>
+
 );
